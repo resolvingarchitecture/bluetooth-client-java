1d4200d218fb001c8c6b141fe6a7a414e6547c0e
objectorange <brian@resolvingarchitecture.io>
2020-11-17 10:00:01 -0500

    added peer discovery

--- src/main/java/ra/bluetooth/BluetoothPeerDiscovery.java
+++ src/main/java/ra/bluetooth/BluetoothPeerDiscovery.java
@@ -1,4 +1,36 @@
 package ra.bluetooth;
 
-public class BluetoothPeerDiscovery {
+import ra.common.Envelope;
+import ra.common.network.NetworkPeer;
+import ra.common.network.NetworkService;
+import ra.util.tasks.BaseTask;
+import ra.util.tasks.TaskRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BluetoothPeerDiscovery extends BaseTask {
+
+    private BluetoothService service;
+
+    public BluetoothPeerDiscovery(BluetoothService service, TaskRunner taskRunner) {
+        super(BluetoothPeerDiscovery.class.getSimpleName(), taskRunner);
+        this.service = service;
+    }
+
+    @Override
+    public Boolean execute() {
+        running = true;
+        List<NetworkPeer> peers = new ArrayList<>();
+        peers.addAll(service.peersInDiscovery.values());
+        for(NetworkPeer destination : peers) {
+            Envelope e = Envelope.commandFactory();
+            // Add in reverse order as underlying routing slip is a stack
+            e.addExternalRoute(BluetoothService.class.getName(), NetworkService.OPERATION_PEER_STATUS_REPLY, destination, service.getNetworkState().localPeer);
+            e.addExternalRoute(BluetoothService.class.getName(), NetworkService.OPERATION_PEER_STATUS, service.getNetworkState().localPeer, destination);
+            service.sendOut(e);
+        }
+        running = false;
+        return true;
+    }
 }
